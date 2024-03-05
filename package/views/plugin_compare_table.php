81d2086f0757a6f1ed1d0b2b2a9bd531659698e1
CreativeMinds <160736600+creativemindsplugins@users.noreply.github.com>
2024-03-05 20:43:02 +0300

    Add files via upload

---
+++ package/views/plugin_compare_table.php
@@ -0,0 +1,228 @@
+<style>
+    .cm-upgrade-table {
+        border-collapse: collapse;
+    }
+
+    .cm-upgrade-table td {
+        padding: 6px;
+    }
+
+    .cm-upgrade-table .tr-head td {
+        position: relative;
+    }
+
+    .cm-upgrade-table .tr-head td .dashicons {
+        position: relative;
+        top: 5px;
+    }
+
+    .cm-upgrade-table tr:not(.tr-head) td:first-child {
+        text-align: left;
+        padding: 6px 40px;
+    }
+
+    .ui-widget-shadow {
+        opacity: 1 !important;
+    }
+    .ui-widget-shadow {
+        background: #fff !important;
+    }
+
+</style>
+
+<h2 style="padding-left:10px;">Upgrade The Header and Footer Script Loader Plugin:</h2>
+
+<table class="cm-upgrade-table"
+       style="background:#fff; width: 100%; border:1px solid black; border-right:1px solid black; border-left:1px solid black;">
+    <tr class="tr-head">
+        <td style="text-align:center; background-color:lightsteelblue; font-size:20px; border-right: 1px solid #c0c0c0; width:40%">
+            <strong>PLANS</strong></td>
+        <td style="text-align:center; background-color:red; font-size:20px; color:white; width:30%">
+            <strong>Current Plan</strong></td>
+        <td style="text-align:center; background-color:green; font-size:20px; color:white; width:30%"><span class="dashicons dashicons-star-filled" style="color:yellow"></span> <strong>Pro</strong></td>
+    </tr>
+    <tr>
+        <td style="text-align:center; border-right: 1px solid #c0c0c0; width:40%"></td>
+        <td style="text-align:center; font-size:20px; width:30%">FREE</td>
+        <td style="text-align:center; font-size:20px; width:30%">$29</td>
+    </tr>
+    <tr>
+        <td style="border-right: 1px solid #c0c0c0; width:40%">Price includes 1 year support/updates. Manual renewal with 40% discount, not a subscription 
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="We do not store credit card information or bill without your consent. The product(s) will remain fully functional even after the license expires."></span></td>
+        <td style="text-align:center; width:30%"></td>
+        <td style="text-align:center; width:30%"><a href="https://www.cminds.com/?edd_action=add_to_cart&download_id=59678&edd_options[price_id]=1&utm_source=scriptloaderfree&utm_campaign=freeupgrade&upgrade=1" class="button button-primary" style="background-color:green;" title="Click to Buy PRO">Upgrade NOW</a></td>
+    </tr>
+    <tr>
+        <td style="text-align:center; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; width:40%"></td>
+        <td style="text-align:center; font-size:16px; border-bottom: 1px solid #c0c0c0; width:30%"></td>
+        <td style="text-align:center; font-size:16px; border-bottom: 1px solid #c0c0c0; width:30%"><a href="https://www.cminds.com/wordpress-plugins-library/wordpress-header-and-footer-script-loader-plugin/?utm_source=scriptloaderfree&utm_campaign=freeupgrade&upgrade=1" target="_blank">Details >></a></td>
+    </tr>
+    <tr>
+        <td style="border-right: 1px solid #c0c0c0; border-top: 1px solid #c0c0c0; width:40%">Number of Websites / License Activations 
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="On how many websites this product plan can be installed."></span></td>
+        <td style="text-align:center; font-size:16px; width:30%"></td>
+        <td style="text-align:center; font-size:16px; width:30%">1 Site</td>
+    </tr>
+    <tr style="height:50px">
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-top: 1px solid #c0c0c0; font-size:16px;" colspan="3">
+            <strong>BASIC FEATURES</strong>
+        </td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Unlimited scripts and styles
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="Manage multiple JSS scripts and CSS styles across your site."></span>
+        </td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Load in the header or in footer
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="Per each script or style you can decide if to upload it on the header section (before or after the body tag) or the footer sections (before the closing body tag)."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Manually approve each script and style
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="In each post you have a metabox from which you can control which script or style is loaded on your WordPress website."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Edit Headers & Footer Content
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="You can add custom headers and edit headers or footer scripts."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr style="height:50px">
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; font-size:16px;" colspan="3">
+            <strong>POST TYPES SUPPORT</strong>
+        </td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Supports posts and pages
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="Enable each script on all post or all pages"></span>
+        </td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Supports custom post types
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="Insert headers and footers on any post or pages and also on custom posts."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Ability to override default settings
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="In each post you can override global setting for a specific style or script and decide if to upload the script or not."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr style="height:50px">
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; font-size:16px;" colspan="3">
+            <strong>TARGET FEATURES</strong>
+        </td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Target by post type
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="You can target scripts and styles to a specific group of post by post type."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Target script by URL
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="This classic footer & header WordPress plugin lets you target a script or style to load on a specific URL or a group of URLs even if you can not edit the page of this URL."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0;">
+            Target by device
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="You can target desktop only, mobile only or both for each script you set."></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr style="height:50px">
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; font-size:16px;" colspan="3">
+            <strong>SUPPORT</strong>
+        </td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; background-color:lightgreen;">
+            Product Knowledge Base
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="We offer comprehensive knowledge base articles that cover all product features and use cases in detail"></span>
+        </td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0; background-color:lightgreen;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0; background-color:lightgreen;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; background-color:lightgreen;">
+            Priority email support
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="We strive to address tickets within a 48-hour timeframe and offer on-site support when needed."></span>
+        </td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0; background-color:lightgreen;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0; background-color:lightgreen;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr>
+        <td style="width:40%; border-bottom: 1px solid #c0c0c0; border-right: 1px solid #c0c0c0; background-color:lightgreen;">
+            Product updates
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="We regularly release new product features and maintenance updates."></span>
+        </td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0; background-color:lightgreen;"><span class="dashicons dashicons-no" style="color:red"></span></td>
+        <td style="text-align:center; width:30%; border-bottom: 1px solid #c0c0c0; background-color:lightgreen;"><span class="dashicons dashicons-yes" style="color:green"></span></td>
+    </tr>
+    <tr class="tr-head">
+        <td style="text-align:center; background-color:lightsteelblue; font-size:20px; border-right: 1px solid #c0c0c0; width:40%">
+            <strong>PLANS</strong></td>
+        <td style="text-align:center; background-color:red; font-size:20px; color:white; width:30%">
+            <strong>Current Plan</strong></td>
+        <td style="text-align:center; background-color:green; font-size:20px; color:white; width:30%"><span class="dashicons dashicons-star-filled" style="color:yellow"></span> <strong>Pro</strong></td>
+    </tr>
+    <tr>
+        <td style="border-right: 1px solid #c0c0c0; border-bottom: 1px solid #c0c0c0; width:40%">Number of Websites / License Activations 
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="On how many websites this product plan can be installed."></span></td>
+        <td style="text-align:center; border-bottom: 1px solid #c0c0c0; font-size:16px; width:30%"></td>
+        <td style="text-align:center; border-bottom: 1px solid #c0c0c0; font-size:16px; width:30%">1 Site</td>
+    </tr>
+    <tr>
+        <td style="text-align:center; border-right: 1px solid #c0c0c0; width:40%"></td>
+        <td style="text-align:center; font-size:20px; width:30%">FREE</td>
+        <td style="text-align:center; font-size:20px; width:30%">$29</td>
+    </tr>
+    <tr>
+        <td style="border-right: 1px solid #c0c0c0; width:40%">Price includes 1 year support/updates. Manual renewal with 40% discount, not a subscription 
+            <span class="dashicons dashicons-info-outline cminds-package-show-tooltip" style="color:green"
+                  title="We do not store credit card information or bill without your consent. The product(s) will remain fully functional even after the license expires."></span></td>
+        <td style="text-align:center; width:30%"></td>
+        <td style="text-align:center; width:30%"><a href="https://www.cminds.com/?edd_action=add_to_cart&download_id=59678&edd_options[price_id]=1&utm_source=scriptloaderfree&utm_campaign=freeupgrade&upgrade=1" class="button button-primary" style="background-color:green;" title="Click to Buy PRO">Upgrade NOW</a></td>
+    </tr>
+    <tr>
+        <td style="text-align:center; border-right: 1px solid #c0c0c0; width:40%"></td>
+        <td style="text-align:center; font-size:16px; width:30%"></td>
+        <td style="text-align:center; font-size:16px; width:30%"><a href="https://www.cminds.com/wordpress-plugins-library/wordpress-header-and-footer-script-loader-plugin/?utm_source=scriptloaderfree&utm_campaign=freeupgrade&upgrade=1" target="_blank">Details >></a></td>
+    </tr>
+</table>
---
+++ readme.txt
@@ -0,0 +1,106 @@
+=== CM Header & Footer Script Loader - Insert Script Plugin ===
+Name: CM Header & Footer Script Loader
+Contributors: CreativeMindsSolutions
+Donate link: https://www.cminds.com/
+Author URI: https://www.cminds.com/
+Tags: footer, header, custom css, insert javascript, insert HTML
+Requires at least: 5.4.0
+Tested up to: 6.4
+Stable Tag: 1.1.9
+
+Easily control header and footer scripts and styles on your WordPress site with a user-friendly plugin
+
+== Description ==
+
+[Premium Plugin Page](https://www.cminds.com/wordpress-plugins-library/wordpress-header-and-footer-script-loader-plugin/) | [Videos](https://www.videolessonsplugin.com/video-lesson/lesson/header-footer-script-loader-plugin/)  | [User Guide](https://creativeminds.helpscoutdocs.com/category/452-header-and-footer-script-loader) 
+
+**Easily manage and control header and footer scripts and styles on your WordPress site using a simple and user-friendly plugin.**
+
+With the Header and Footer Script Loader plugin, you can easily manage and control additional scripts and styles on your WordPress site. 
+
+This plugin allows you to pre-load all scripts and styles in the plugin settings, as well as inject custom JavaScript and code to suit your needs.
+
+You have the option to choose whether a script should be automatically or manually uploaded in all posts. 
+
+### Insert Headers and Footers Premium Edition
+
+The [Insert Headers and Footers Plugin Premium editions](https://www.cminds.com/wordpress-plugins-library/wordpress-header-and-footer-script-loader-plugin/) includes even more powerful features, such as: each post has a header and footer metabox that allows you to control the various scripts and styles, device selection for each script and stlye and much more.
+
+### Insert Headers and Footers Plugin Introduction Video
+
+[vimeo https://vimeo.com/141020978]
+
+== Insert Script Plugin Use Cases == 
+
+* **Control Site Scripts** – Easily add or replace various scripts and styles to your WordPress site
+* **Selective Loading Per Post** – Target scripts and styles to specific posts or groups of custom posts
+* **Reduce Script Load** – Reduce the amount of scripts loaded throughout the site
+* **Target Script to Specific Posts or URL Pattern** (premium plugin) – Target scripts to load on a specific URL or URL pattern
+* **Per Device Script Support** (premium plugin) – Load script on a Mobile or Desktop or both
+
+== Basic Plugin Features == 
+
+* Add unlimited number of scripts and styles.
+* Manage which scripts load on header or footer.
+* Manage which scripts load of post or pages.
+
+== Pro Version Features == 
+
+> [Pro Version Detailed Features List](https://www.cminds.com/wordpress-plugins-library/wordpress-header-and-footer-script-loader-plugin/)
+>
+> * Support custom post types.
+> * Support script loading on custom posts.
+> * Target scripts and styles to a specific group of post by post type.
+> * Control script loading on specific posts using a metabox.
+> * Override global setting for a specific style or script and decide if to upload the script or not.
+> * Target script by URL or group of URLs
+> * Target by device type: desktop only, mobile only or both for each script you set.
+> * Add custom headers and edit headers or footer scripts.
+
+
+== Follow Us ==
+
+[Blog](https://www.cminds.com/category/wordpress/) | [Twitter](http://twitter.com/cmplugins) | [LinkedIn](https://www.linkedin.com/company/creativeminds) | [YouTube](https://www.youtube.com/user/cmindschannel) | [Pinterest](http://www.pinterest.com/cmplugins/) | [FaceBook](https://www.facebook.com/cmplugins/)
+
+== Plugin usage instructions ==
+
+1. Fill the "Script Name" field and paste "Script Code"
+2. Select "Script Type" (CSS or JS), "Script Location" (header or footer), and "Autoload Option"
+3. Check "Custom Post Types" on which script should be loaded if necessary
+4. Click "Save Changes"
+
+== Suggested Plugins by CreativeMinds ==
+
+List of all [WordPress Plugins](https://www.cminds.com) by CreativeMinds
+
+* [CM ToolTip Glossary](https://glossaryplugin.com/) - Easily creates a Glossary, Encyclopaedia or Dictionary of your website's terms and shows them as a tooltip in posts and pages when hovering. With many more powerful features.
+* [CM Download Manager](https://www.downloadmanagerplugin.com/) - Allows users to upload, manage, track and support documents or files in a download directory listing database for others to contribute, use and comment upon.
+* [CM Answers Plugin](https://www.answersplugin.com/) - A fully-featured WordPress Questions & Answers Plugin that allows you to build multiple discussion forum systems Just like StackOverflow, Yahoo Answers and Quora, Now with MicroPayment and Anonymous posting support!
+* [CM Restrict Content](http://restrictcontent.com/) - A full-featured, powerful membership solution and content restriction plugin for WordPress. Support access by role to content on your site.
+* [CM OnBoarding](https://onboardingplugin.com/) - Superb Guidance tool which improves the online experience and the user satisfaction.
+
+== Screenshots ==
+
+1. Plugin settings when loading on specific custom posts (pro)
+2. Adding multiple scripts in plugin settings
+3. Metabox control on specific posts
+
+== Installation ==
+
+1. Upload `cm-onboarding` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
+= 1.1.9 =
+Release Date: 22 December 2023
+* Update package info
+
+= 1.1.8 =
+Release Date: 21st Mar 2023
+* Update package info
+
+= 1.1.7 =
+Release Date: 9th Nov 2022
+* Replace Internal support links
+
+> [View Release Notes in Plugin Site](https://www.cminds.com/wordpress-plugins-library/wordpress-header-and-footer-script-loader-plugin/#changelog)
